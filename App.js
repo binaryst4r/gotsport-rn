2361e7c62b11e9903d1e7e159abb016e569db8d0
Larry Gust <larry.v.gust@gmail.com>
2022-07-18 23:47:50 -0600

    initial commit webview

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { WebView } from 'react-native-webview';
+import { SafeAreaView } from "react-native";
+import {LOCALHOST_IP} from '@env'
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1, marginTop: 28 }}>
+      <StatusBar style={'auto'} />
+      <View style={{height: '100%', width: '100%'}}>
+        <WebView 
+          style={styles.container}
+          source={{ uri: LOCALHOST_IP }}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
